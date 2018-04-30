0293799997e8119691ec81c70fac7dcb1ef7d09a
chandu <chandrasr.palle@gmail.com>
2018-04-30 23:56:14 +0200

    indentation changed and corrections added after code review

--- week5/step-1.js
+++ week5/step-1.js
@@ -1,124 +1,120 @@
-
 console.log("-------------------- step 1.1 -------------------------");
 stepOne(10, 15, sayThree, sayFive);
 
-function stepOne(startVal, endVal, callbackThree, callbackFive){
-        let generatedArray = [];
-        for( let i = startVal; startVal <= endVal; startVal++){
-            generatedArray.push(startVal);
-        }
+function stepOne(startVal, endVal, callbackThree, callbackFive) {
+  let generatedArray = [];
+  for (let i = startVal; startVal <= endVal; startVal++) {
+    generatedArray.push(startVal);
+  }
 
-        generatedArray.forEach(function(element){
-            if(element % 3 == 0){
-                console.log("--> callback to callbackThree function");
-                callbackThree(element);
-            }
-            if(element % 5 == 0){
-                console.log("--> callback to callbackFive function");
-                callbackFive(element);
-            }
-        });
+  generatedArray.forEach(function(element) {
+    if (element % 3 == 0) {
+      console.log("--> callback to callbackThree function");
+      callbackThree(element);
+    }
+    if (element % 5 == 0) {
+      console.log("--> callback to callbackFive function");
+      callbackFive(element);
+    }
+  });
 }
-function sayThree(number){
-        console.log(`${number} is divided by three`);
+function sayThree(number) {
+  console.log(`${number} is divided by three`);
 }
-function sayFive(number){
-        console.log(`${number} is divided by five`);
+function sayFive(number) {
+  console.log(`${number} is divided by five`);
 }
-
 
 console.log("-------------------- step 1.3 -------------------------");
 console.log(repeatStringNumTimes("abc", 3));
 function repeatStringNumTimes(str, num) {
-    // repeat after me
-    let res = "";
-    if(num >0){
-      for(let i=1; i <=num; i++){
-        res +=str;
-      }
+  // repeat after me
+  let res = "";
+  if (num > 0) {
+    for (let i = 1; i <= num; i++) {
+      res += str;
     }
-    return res;
   }
+  return res;
+}
 
 console.log("-------------------- step 1.4 -------------------------");
 console.log(repeatStringNumTimes("abc", 3));
 function repeatStringNumTimes(str, num) {
-    // repeat after me
-    let res = "";
-    if(num >0){
-        while(num>0){
-            res += str;
-            num--;
-        }
+  // repeat after me
+  let res = "";
+  if (num > 0) {
+    while (num > 0) {
+      res += str;
+      num--;
     }
-    return res;
   }
+  return res;
+}
 console.log("-------------------- step 1.5 -------------------------");
 console.log(repeatStringNumTimes("abc", 3));
 // Using a For loop
 function repeatStringNumTimes(str, num) {
-    // repeat after me
-    let res = "";
-    if(num >0){
-        do{
-            res += str;
-            num--;
-        }while(num>0)
-    }
-    return res;
+  // repeat after me
+  let res = "";
+  if (num > 0) {
+    do {
+      res += str;
+      num--;
+    } while (num > 0);
   }
+  return res;
+}
 console.log("-------------------- step 1.6 -------------------------");
 let Car = function() {
-    this.wheels = 4;
-    this.engines = 1;
-    this.seats = 5;
-  };
-  
-  // Only change code below this line.
-  let MotorBike = function() {
-    this.wheels = 2;
-    this.engines = 1;
-    this.seats = 2;
-  
-  };
-  console.log("Constructor motorbike is --> " + MotorBike);
+  this.wheels = 4;
+  this.engines = 1;
+  this.seats = 5;
+};
+
+// Only change code below this line.
+let MotorBike = function() {
+  this.wheels = 2;
+  this.engines = 1;
+  this.seats = 2;
+};
+console.log("Constructor motorbike is --> " + MotorBike);
 
 console.log("-------------------- step 1.7 -------------------------");
 function multiplyAll(arr) {
-    let product = 1;
-    // Only change code below this line
-    for (let i = 0; i < arr.length; i++){
-      for(let j = 0; j < arr[i].length;j++){
-        product *= arr[i][j];
-      }
+  let product = 1;
+  // Only change code below this line
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j];
     }
-    // Only change code above this line
-    return product;
   }
-  
-  // Modify values below to test your code
- console.log(multiplyAll([[1,2],[3,4],[5,6,7]]));
+  // Only change code above this line
+  return product;
+}
+
+// Modify values below to test your code
+console.log(multiplyAll([[1, 2], [3, 4], [5, 6, 7]]));
 
 // -------------------- step 1.8 -------------------------
- 
+
 // -------------------- step 1.9 -------------------------
-  
-let x = 9; 
-function f1(val) { 
-    val = val+1; 
-    return val;
+
+let x = 9;
+function f1(val) {
+  val = val + 1;
+  return val;
 }
 f1(x);
 console.log(x);
 
-
 let y = { x: 9 };
 function f2(val) {
-    val.x = val.x + 1;
-    return val;
+  val.x = val.x + 1;
+  return val;
 }
 f2(y);
-console.log(y);  
-console.log("In case of varibles values get passes but when pass objects reference get passed.");
-
-
+console.log(y);
+console.log(
+  "In case of primitive data type values get passed but when in case of objects reference get passed."
+);
--- week5/step-3.js
+++ week5/step-3.js
@@ -3,60 +3,47 @@ let btnEnter = document.querySelector(".btn-enter");
 let nodeJs = document.querySelector(".btn-node");
 let inputRepo = document.querySelector(".input-repo");
 
-btnStepThree.addEventListener('click',function(){
-    console.log("you clicked me!");
-    showHYFRepos('ALL');
+btnStepThree.addEventListener("click", function() {
+  console.log("you clicked me!");
+  showHYFRepos("ALL");
 });
-btnEnter.addEventListener('click',function(){
-    showHYFRepos(inputRepo.value);
+btnEnter.addEventListener("click", function() {
+  showHYFRepos(inputRepo.value);
 });
-nodeJs.addEventListener('click',function(){
-    showHYFRepos('Node.js');
+nodeJs.addEventListener("click", function() {
+  showHYFRepos("Node.js");
 });
 
 function fetchJSONData(url, callbackFn) {
-    const xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', function() {
-        console.log("Data loaded.");
-        const data = JSON.parse(xhr.responseText);
-        callbackFn(data);
-    });
-    xhr.open('GET', url);
-    xhr.send();
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", function() {
+    console.log("Data loaded.");
+    const data = JSON.parse(xhr.responseText);
+    callbackFn(data);
+  });
+  xhr.open("GET", url);
+  xhr.send();
 }
 function showHYFRepos(toDisplay) {
-    fetchJSONData('https://api.github.com/orgs/HackYourFuture/repos', function(repoList) {
-    let repoToDisplay = [];  
-    if(toDisplay === 'ALL'){
-        repoToDisplay = repoList;
-      }
-      else{
-        repoToDisplay = repoList.filter((element) => { return element.name == toDisplay });
-      }
-      const ul = document.querySelector('#repoList');
-      ul.innerHTML = "";
-    
-      repoToDisplay.forEach(repo => {
-        const li = document.createElement('li');
-        li.innerHTML = '<a target="_blank" href=' + repo.html_url + '> ' + repo.name + ' </a>';
-        ul.appendChild(li);          
-      }); 
-    });        
+  fetchJSONData("https://api.github.com/orgs/HackYourFuture/repos", function(
+    repoList
+  ) {
+    let repoToDisplay = [];
+    if (toDisplay === "ALL") {
+      repoToDisplay = repoList;
+    } else {
+      repoToDisplay = repoList.filter(element => {
+        return element.name == toDisplay;
+      });
+    }
+    const ul = document.querySelector("#repoList");
+    ul.innerHTML = "";
+
+    repoToDisplay.forEach(repo => {
+      const li = document.createElement("li");
+      li.innerHTML =
+        '<a target="_blank" href=' + repo.html_url + "> " + repo.name + " </a>";
+      ul.appendChild(li);
+    });
+  });
 }
-
-    //   else if(toDisplay == 'Node'){
-    //     repoToDisplay = repoList.filter((element) => { return element.name == 'Node.js' });
-    //   }
-    //   else{
-    //       repoToDisplay = repoList;
-    //   }
-
-
-
-        // const a = document.createElement('a');
-        // a.href="https://api.github.com/repos/HackYourFuture/" + repo.name;
-        // a.textContent = repo.name;
-        // li.appendChild(a);
-
-
-
