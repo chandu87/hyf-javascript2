508f1d31b7714c856c2429aa54fd809e2c3dfbef
chandu <chandrasr.palle@gmail.com>
2018-05-03 16:16:10 +0200

    setp3.7 added

--- week6/week.js
+++ week6/week.js
@@ -1,8 +1,12 @@
 
 const btnGetData = document.querySelector(".btn-get-data");
+const serachRepo = document.querySelector(".search-repo");
 
+showHYFRepos();
 btnGetData.addEventListener('click',function(){
-    showHYFRepos();
+   
+    const query = serachRepo.value;
+    searchHyfRepos(query);
 });
 function fetchJSONData(url, callbackFn) {
     const xhr = new XMLHttpRequest();
@@ -27,3 +31,42 @@ function fetchJSONData(url, callbackFn) {
     });
 }
 
+function searchHyfRepos(searchText){
+
+const repoSearchUrl = 'https://api.github.com/search/repositories?q=user:HackYourFuture+' + searchText;
+
+getAjaxData(repoSearchUrl,function(searchedRepoData){
+
+    const ulList = document.querySelector(".ul-list");
+    ulList.innerHTML = "";
+    searchedRepoData.items.forEach(element => {
+        const liItem = document.createElement('li');
+        liItem.innerHTML = `<a href="${element.url}">${element.name}</a>`;
+        ulList.appendChild(liItem);
+    });
+});
+
+}
+
+function getAjaxData(url, callback) {
+    // Create new ajax call with the js function called XMLHttpRequest
+    const request = new XMLHttpRequest();
+    request.addEventListener('load', function () {
+        // This in here is our callback function
+        // Check our server responsecode, 200 means ok, success: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes 
+        if (this.status === 200) {
+            callback(JSON.parse(request.responseText));
+        } else {
+            console.log('Something is probably wrong with the url');
+        }
+    });
+
+    request.addEventListener('error', function () {
+        console.log('Server error like timeout');
+    });
+
+    // initializes a request with an http method
+    request.open("GET", url);
+    // Sends the request 
+    request.send();
+}
