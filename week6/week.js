674329080c42275f8dfa8b67af006a64e45b11f2
chandu <chandrasr.palle@gmail.com>
2018-05-04 19:38:28 +0200

    step 4 added

--- week6/week.js
+++ week6/week.js
@@ -9,9 +9,9 @@ btnGetData.addEventListener('click',function(){
     searchHyfRepos(query);
 });
 
-// Function for retrieving data from HYF Repos
+// Function for retrieving ALL HYF Repos
 function showHYFRepos() {
-    getAjaxData("https://api.github.com/orgs/HackYourFuture/repos", displayData);
+    getAjaxData("https://api.github.com/orgs/HackYourFuture/repos", displayData, displayError);
 }
 
 // Function for retriving searched Repo List from HYF
@@ -20,10 +20,10 @@ function searchHyfRepos(searchText){
         const repoSearchUrl = 'https://api.github.com/search/repositories?q=user:HackYourFuture+' + searchText;
         getAjaxData(repoSearchUrl, function(searchedData){
             displayData(searchedData.items);
-        });
+        }, displayError);
     }
     else{
-        message.textContent = "Please Enter text in Input field and Click the button";
+        message.textContent = "We could not perform this search. Search text is required when searching. Were you searching for something else.";
     }
 
 
@@ -33,10 +33,36 @@ function searchHyfRepos(searchText){
 function displayData(data){
     if(data.length > 0){
     const ulList = document.querySelector(".ul-list");
+    const contributorsContainer = document.querySelector(".contributors-container");    
     ulList.innerHTML = "";
+
     data.forEach(element => {
         const liItem = document.createElement('li');
-        liItem.innerHTML = `<a href="${element.url}">${element.name}</a>`;
+        liItem.innerHTML = `<a>${element.name}</a>`;
+        liItem.addEventListener('click',function(){
+            contributorsContainer.innerHTML = "";
+            const repoName = document.createElement('h2');
+            repoName.innerHTML = `Repository Name: <a href="${element.url}">${element.name}</a>`;
+            contributorsContainer.appendChild(repoName);
+            const contributorHeader = document.createElement('h4');
+            contributorHeader.innerHTML = "Contributors";
+            contributorsContainer.appendChild(contributorHeader);
+
+            getAjaxData(element.contributors_url,function(contributorsData){
+                const contributorsList = document.createElement('ul');
+                contributorsList.innerHTML = "";
+                contributorsContainer.appendChild(contributorsList);
+                contributorsData.forEach((contributor)=>{
+                    const contributorListItem = document.createElement('li');
+                    contributorListItem.innerHTML = contributor.login;
+                    contributorsList.appendChild(contributorListItem);
+                    const img = document.createElement('img');
+                    img.src = contributor.avatar_url;
+                    contributorsList.appendChild(img);
+
+                });
+            });
+        });
         ulList.appendChild(liItem);
         });
     }
@@ -44,22 +70,26 @@ function displayData(data){
         message.textContent = "There are no matched Results. Try again!!"
     }
 }
+function displayError(error){
+    message.textContent = error;
+}
 
-function getAjaxData(url, callback) {
+// function for getting data with given URL
+function getAjaxData(url, successCallback, failureCallback) {
     // Create new ajax call with the js function called XMLHttpRequest
     const request = new XMLHttpRequest();
     request.addEventListener('load', function () {
         // This in here is our callback function
         // Check our server responsecode, 200 means ok, success: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes 
         if (this.status === 200) {
-            callback(JSON.parse(request.responseText));
+            successCallback(JSON.parse(request.responseText));
         } else {
-            console.log('Something is probably wrong with the url');
+            failureCallback('Something is probably wrong with the url');
         }
     });
 
     request.addEventListener('error', function () {
-        console.log('Server error like timeout');
+        failureCallback('Server error, Failed to load');
     });
 
     // initializes a request with an http method
@@ -67,3 +97,17 @@ function getAjaxData(url, callback) {
     // Sends the request 
     request.send();
 }
+
+
+//------------------------- Bonus task ------------------------
+function scanDuplicates(array){
+    let newArray = [];
+    array.forEach(element =>{
+        if(!newArray.includes(element)){
+            newArray.push(element);
+       }
+    });
+    return newArray;
+}
+const testArray = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c','a','b','c',"a"]; 
+console.log(scanDuplicates(testArray));
